a929b8bfef4115081ac046dbebb9f7835f78a1b5
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-03-10 18:41:34 -0400

    Modelo creado.

--- Marcos_Rosario_P2_AP1/Models/Modelos.cs
+++ Marcos_Rosario_P2_AP1/Models/Modelos.cs
@@ -1,6 +1,16 @@
-﻿namespace Marcos_Rosario_P2_AP1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Marcos_Rosario_P2_AP1.Models
 {
 	public class Modelos
 	{
+		[Key]
+		public int CiudadId { get; set; }
+
+		public string Nombre { get; set; }
+
+		public double Monto { get; set; }
+
+
 	}
 }
