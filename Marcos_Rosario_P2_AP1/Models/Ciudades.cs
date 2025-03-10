f19260c8e395986b5ce1367008995a8b36b20455
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-03-10 19:40:52 -0400

    Ciudades fue modificado.

---
+++ Marcos_Rosario_P2_AP1/Models/Ciudades.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Marcos_Rosario_P2_AP1.Models
+{
+	public class Ciudades
+	{
+		[Key]
+		public int CiudadId { get; set; }
+
+		public string Nombre { get; set; }
+
+		public double Monto { get; set; }
+
+		[InverseProperty("Ciudades")]
+		public virtual ICollection<Cursos> Cursos { get; set; } = new List<Cursos>();
+	}
+}
