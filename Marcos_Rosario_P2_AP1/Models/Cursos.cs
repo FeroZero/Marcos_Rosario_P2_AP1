690f90bb571e4de1bbf4d709dd43a3130799797f
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-03-10 19:04:17 -0400

    Modelo Cursos fue creado.

---
+++ Marcos_Rosario_P2_AP1/Models/Cursos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Marcos_Rosario_P2_AP1.Models
+{
+	public class Cursos
+	{
+		[Key]
+		public int CursoId { get; set; }
+
+		[Required]
+		public DateOnly Fecha { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+		[Required]
+		[RegularExpression(@"^[a-zA-Z1-9\s]+$", ErrorMessage = "Caracteres no permitidos")]
+		[StringLength(50,ErrorMessage = "Limite Excedido.")]
+		public string Asignatura { get; set; }
+
+		[Required]
+		[Range(0, 1000000,ErrorMessage = "Limite Excedido.")]
+		public double Monto { get; set; }
+	}
+}
