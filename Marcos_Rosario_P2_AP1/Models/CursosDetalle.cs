b608d6440119e2285e65bf9d2f6b8febc9b33fda
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2025-03-10 19:40:42 -0400

    El detalle de cursos fue creado.

---
+++ Marcos_Rosario_P2_AP1/Models/CursosDetalle.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Marcos_Rosario_P2_AP1.Models
+{
+	public class CursosDetalle
+	{
+		[Key]
+		public int DetalleId { get; set; }
+
+		public int CursoId { get; set; }
+
+		public int CiudadId { get; set; }
+
+		public double Valor { get; set; }
+
+		[ForeignKey("CursoId")]
+		[InverseProperty("CursosDetalle")]
+		public virtual Cursos Cobro { get; set; } = null!;
+	}
+}
